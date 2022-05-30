ddbd9fb434ee68d06dd09c9a5ca7ca531eda262b
Son Hong Do <sondh0127@gmail.com>
2022-05-31 00:06:42 +0700

    init validation and transformer

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 
 async function createApp() {
   const app = await NestFactory.create(AppModule);
+
+  // 👇 binds ValidationPipe to the entire application
+  app.useGlobalPipes(new ValidationPipe());
+
+  // 👇 apply transform to all responses
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
 
   const config = new DocumentBuilder()
     .setTitle('NestJS Prisma Vite')
